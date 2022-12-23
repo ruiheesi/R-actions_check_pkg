b025a6b05ecb167cb46858144581181f5b92f692
ruiheesi <107956586+ruiheesi@users.noreply.github.com>
2022-12-22 19:08:34 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ apt-get install -y --no-install-recommends \
   r-doc-html=${R_version} \
   r-base-dev=${R_version};
 
-apt-get install -y r-base r-base-dev r-cran-xml r-cran-rjava libcurl4-openssl-dev
+apt-get install -y r-cran-xml r-cran-rjava libcurl4-openssl-dev
 apt-get install -y libssl-dev libxml2-dev openjdk-7-* libgdal-dev libproj-dev libgsl-dev
 apt-get install -y xml2 default-jre default-jdk mesa-common-dev libglu1-mesa-dev freeglut3-dev 
 apt-get install -y mesa-common-dev libx11-dev r-cran-rgl r-cran-rglpk r-cran-rsymphony r-cran-plyr 
